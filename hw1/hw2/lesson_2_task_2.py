baf19def6e8444fcaffbc0c13aa883792c8b81c9
GrigorievaT <tatyana_grigorieva@mail.ru>
2023-06-18 16:11:02 +0500

    #Задание 2

---
+++ hw1/hw2/lesson_2_task_2.py
@@ -0,0 +1,11 @@
+def is_year_leap(year):
+    if year % 4 == 0:
+        print("год ", year, ":", True)
+    else:
+        print("год ", year, ":", False)
+
+year_st=input("Введите год: ")
+year=int(year_st)
+is_year_leap(year) 
+
+   
