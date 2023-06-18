6ebe483415dad2b81f39b8ec8341d4aefadc442f
GrigorievaT <tatyana_grigorieva@mail.ru>
2023-06-18 17:14:22 +0500

    #Задание 5

---
+++ hw1/hw2/lesson_2_task_5.py
@@ -0,0 +1,15 @@
+def month_to_season(num):
+    if num > 12 or num < 1:
+        print("Введите корректный номер месяца (число от 1 до 12)")
+    if num == 1 or num == 2 or num ==12:
+        print("Зима")
+    elif num == 3 or num == 4 or num == 5:
+        print("Весна")
+    elif num == 6 or num == 7 or num == 8:
+        print("Лето")
+    elif num == 9 or num == 10 or num == 11:
+        print("Осень")
+
+n=input("Введите номер месяца, мы определим сезон: ")
+num=int(n)
+month_to_season(num)
