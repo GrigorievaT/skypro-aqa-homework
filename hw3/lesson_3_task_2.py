6dfa0e7700ab260090d529ff65fcd8acba501001
GrigorievaT <tatyana_grigorieva@mail.ru>
2023-06-26 16:41:32 +0500

    #2 изменения

--- hw3/lesson_3_task_2.py
+++ hw3/lesson_3_task_2.py
@@ -1,16 +1,14 @@
 #Задание 2. Список объектов
 from smartphone import Smartphone
-el = Smartphone("Redmi", "Note", "+79998886655")
-el2 = Smartphone("Nokia", "A10", "+71112223344")
-elem1 = el.marka+"-"+el.model+"."+el.number
-elem2 = el2.marka+"-"+el2.model+"."+el2.number
-elem3 = el.marka+"-"+el2.model+"."+el.number
-elem4 = el2.marka+"-"+el.model+"."+el.number
-elem5 = el.marka+"-"+el.model+"."+el2.number
-catalog = [elem1, elem2, elem3, elem4, elem5]
-for i in range(0, 5):
-    print(catalog [i])
-
+catalog = []
+catalog.append(Smartphone("Redmi", "Note", "+79998887766"))
+catalog.append(Smartphone("Nokia", "A10", "+71112223344"))
+catalog.append(Smartphone("Samsung", "A20", "+72223334455"))
+catalog.append(Smartphone("Apple", "13", "+73334445566"))
+catalog.append(Smartphone("Poco", "F5", "+74445556677"))
+for i in range(len(catalog)):
+    print(catalog[i], end=' ')
+    print()
 
 
 
